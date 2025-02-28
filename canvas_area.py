136869dd34baebb870b10f2f609ff19ab084bc0a
Andrew Sharon <andrewsharon@gmx.com>
2025-02-28 01:01:25 -0500

    Fixed ruler scale wrong calculation. Walls correctly use wall width from settings. FIxed panning works at correct speed at all zoom levels.

--- canvas_area.py
+++ canvas_area.py
@@ -61,8 +61,11 @@ class CanvasArea(Gtk.DrawingArea):
 
         self.draw_grid(cr, width, height)
 
+        base_feet_per_pixel = 60.0 / width
+        wall_thickness_feet = self.config.DEFAULT_WALL_WIDTH / 12
+        wall_pixel_width = wall_thickness_feet / base_feet_per_pixel
         cr.set_source_rgb(0, 0, 0)
-        cr.set_line_width(2 / self.zoom)
+        cr.set_line_width(max(wall_pixel_width * self.zoom, 1.0))
         for wall in self.walls:
             cr.move_to(wall.start[0], wall.start[1])
             cr.line_to(wall.end[0], wall.end[1])
@@ -122,13 +125,23 @@ class CanvasArea(Gtk.DrawingArea):
         if n_press == 1:  # Single click
             if not self.drawing_wall:
                 snapped_x, snapped_y = self.snap_manager.snap_point(canvas_x, canvas_y, canvas_x, canvas_y, self.walls, self.rooms)
-                self.current_wall = Wall((snapped_x, snapped_y), (snapped_x, snapped_y), width=4, height=96)
+                self.current_wall = Wall(
+                    (snapped_x, snapped_y), 
+                    (snapped_x, snapped_y), 
+                    width=self.config.DEFAULT_WALL_WIDTH, 
+                    height=self.config.DEFAULT_WALL_HEIGHT
+                )
                 self.drawing_wall = True
             else:
                 snapped_x, snapped_y = self.snap_manager.snap_point(canvas_x, canvas_y, self.current_wall.start[0], self.current_wall.start[1], self.walls, self.rooms)
                 self.current_wall.end = (snapped_x, snapped_y)
                 self.walls.append(self.current_wall)
-                self.current_wall = Wall((snapped_x, snapped_y), (snapped_x, snapped_y), width=4, height=96)
+                self.current_wall = Wall(
+                    (snapped_x, snapped_y), 
+                    (snapped_x, snapped_y), 
+                    width=self.config.DEFAULT_WALL_WIDTH, 
+                    height=self.config.DEFAULT_WALL_HEIGHT
+                )
         elif n_press == 2:  # Double click
             if self.drawing_wall:
                 snapped_x, snapped_y = self.snap_manager.snap_point(canvas_x, canvas_y, self.current_wall.start[0], self.current_wall.start[1], self.walls, self.rooms)
@@ -148,8 +161,8 @@ class CanvasArea(Gtk.DrawingArea):
 
     def on_drag_update(self, gesture, offset_x, offset_y):
         if self.tool_mode == "panning":
-            delta_x = (self.drag_start_x + offset_x - self.drag_start_x) / self.zoom
-            delta_y = (self.drag_start_y + offset_y - self.drag_start_y) / self.zoom
+            delta_x = offset_x
+            delta_y = offset_y
             self.offset_x = self.last_offset_x + delta_x
             self.offset_y = self.last_offset_y + delta_y
             print(f"Drag offset_x: {offset_x}, offset_y: {offset_y}, zoom: {self.zoom}")
@@ -170,9 +183,9 @@ class CanvasArea(Gtk.DrawingArea):
     def draw_rulers(self, cr, width, height):
         """Draw rulers synced with grid, incrementing by 8 ft, following panning."""
         ruler_size = 20
-        base_feet_per_pixel = 60.0 / width
-        major_spacing = 8 / base_feet_per_pixel * self.zoom
-        minor_spacing = 1 / base_feet_per_pixel * self.zoom
+        base_feet_per_pixel = 60.0 / width  # Same as grid: 60 ft across width
+        major_spacing = 8 / base_feet_per_pixel * self.zoom  # 8 ft in pixels at current zoom
+        minor_spacing = major_spacing / 8  # 1 ft in pixels
 
         grid_left_pixel = -self.offset_x / self.zoom
         grid_top_pixel = -self.offset_y / self.zoom
@@ -207,6 +220,7 @@ class CanvasArea(Gtk.DrawingArea):
         cr.set_line_width(1)
 
         print(f"Major spacing: {major_spacing}, Minor spacing: {minor_spacing}")
+        print(f"Base feet per pixel: {base_feet_per_pixel}")
         print(f"First X pixel: {first_major_x_pixel}, First Y pixel: {first_major_y_pixel}")
         print(f"First grid X feet: {first_grid_x_feet}, First grid Y feet: {first_grid_y_feet}")
 
@@ -215,7 +229,7 @@ class CanvasArea(Gtk.DrawingArea):
                 cr.move_to(x, ruler_size - 10)
                 cr.line_to(x, ruler_size)
                 cr.stroke()
-                feet = int(first_grid_x_feet + ((x - first_major_x_pixel) / major_spacing) * 8)
+                feet = round((x - first_major_x_pixel) / major_spacing) * 8 + first_grid_x_feet
                 cr.move_to(x + 2, ruler_size - 7)
                 cr.show_text(f"{feet} ft")
                 print(f"Horizontal tick at x={x}, feet={feet}")
@@ -232,7 +246,7 @@ class CanvasArea(Gtk.DrawingArea):
                 cr.move_to(ruler_size - 10, y)
                 cr.line_to(ruler_size, y)
                 cr.stroke()
-                feet = int(first_grid_y_feet + ((y - first_major_y_pixel) / major_spacing) * 8)
+                feet = round((y - first_major_y_pixel) / major_spacing) * 8 + first_grid_y_feet
                 cr.move_to(2, y + 10)
                 cr.show_text(f"{feet} ft")
                 print(f"Vertical tick at y={y}, feet={feet}")
