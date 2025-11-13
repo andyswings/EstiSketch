1a39cd008d3b582344b556ced0df9490812e4cbd
Andrew Sharon <andrewsharon@gmx.com>
2025-11-13 15:54:23 -0500

    Remove commented-out code from draw_walls method for cleaner implementation

--- Canvas/wall_room_renderer.py
+++ Canvas/wall_room_renderer.py
@@ -2,19 +2,12 @@
 
 def draw_walls(self, cr):
     cr.set_source_rgb(0, 0, 0) # Black lines.
-    # cr.set_line_width(self.config.DEFAULT_WALL_WIDTH) # Set wall width.
     cr.set_line_join(0) # 0 = miter join.
     cr.set_line_cap(0) # 0 = butt cap.
     cr.set_miter_limit(10.0)
     for wall_set in self.wall_sets:
         if not wall_set:
             continue
-        # cr.move_to(wall_set[0].start[0], wall_set[0].start[1])
-        # for wall in wall_set:
-        #     cr.line_to(wall.end[0], wall.end[1])
-        # if len(wall_set) > 2 and wall_set[-1].end == wall_set[0].start:
-        #     cr.close_path()
-        # cr.stroke()
         first_wall = wall_set[0]
         line_width_user = first_wall.width / self.zoom
         cr.set_line_width(line_width_user)
@@ -27,14 +20,6 @@ def draw_walls(self, cr):
         cr.stroke()
 
     if self.walls:
-        # cr.move_to(self.walls[0].start[0], self.walls[0].start[1])
-        # for wall in self.walls:
-        #     cr.line_to(wall.end[0], wall.end[1])
-        # if self.current_wall:
-        #     cr.line_to(self.current_wall.end[0], self.current_wall.end[1])
-        # if not self.drawing_wall and len(self.walls) > 2 and self.walls[-1].end == self.walls[0].start:
-        #     cr.close_path()
-        # cr.stroke()
         first_loose = self.walls[0]
         line_width_user = first_loose.width / self.zoom
         cr.set_line_width(line_width_user)
@@ -49,9 +34,6 @@ def draw_walls(self, cr):
         cr.stroke()
         
     elif self.current_wall:
-        # cr.move_to(self.current_wall.start[0], self.current_wall.start[1])
-        # cr.line_to(self.current_wall.end[0], self.current_wall.end[1])
-        # cr.stroke()
         line_width_user = self.current_wall.width / self.zoom
         cr.set_line_width(line_width_user)
         cr.move_to(self.current_wall.start[0], self.current_wall.start[1])
